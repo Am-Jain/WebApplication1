03fc9140d781b67c3085184351f3ed998f11a889
Am-Jain <amitj4377@gmail.com>
2020-05-20 23:20:15 +0530

    Initial push

---
+++ EmpoyeeDAL/Models/EmployeeDBContext.cs
@@ -0,0 +1,20 @@
+﻿using EmployeeManagement.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmpoyeeDAL.Models
+{
+    public class EmployeeDBContext: DbContext
+    {
+
+        public EmployeeDBContext(DbContextOptions<EmployeeDBContext> contextOptions) : base (contextOptions)
+        {
+
+        }
+
+        public DbSet<Employee> Employees{ get; set; }
+    }
+}
---
+++ WebApplication3/Employeecontroller.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .controller('Employeecontroller', Employeecontroller);
+
+    Employeecontroller.$inject = ['$location'];
+
+    function Employeecontroller($location) {
+        /* jshint validthis:true */
+        var vm = this;
+        vm.title = 'Employeecontroller';
+
+        activate();
+
+        function activate() { }
+    }
+})();
